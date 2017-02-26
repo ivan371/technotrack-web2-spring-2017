9172a3586d182c09d854eef210d0bb4a1e77ecbb
Ivan <nagaiko.ivan@gmail.com>
2017-02-26 17:45:15 +0300

    rating

--- like/signals.py
+++ like/signals.py
@@ -2,12 +2,24 @@ from ugc.models import Post
 from like.models import Like, Likeable
 from django.db.models.signals import post_save, pre_save, pre_delete
 from django.contrib.contenttypes.models import ContentType
+from django.db.models import Q
 
-def recount_likes_on_post(instance, created = True, *args, **kwargs):
+def recount_rating(num, post):
+    if post.like_count >= num:
+        print("HEL")
+        u = post.author
+        u.rating = (num/10)*Post.objects.filter(
+            Q(author=u) & Q(like_count__gte=num)).count()
+        u.save()
+
+def recount_likes_on_post(instance, created = False, *args, **kwargs):
 
      if created:
         instance.target.like_count = Like.objects.filter(target_id = instance.target_id).count()
         instance.target.save()
+        recount_rating(1000, instance.target)
+        recount_rating(100, instance.target)
+        recount_rating(10, instance.target)
 
 def delete_like(instance, *args, **kwargs):
 
--- ugc/tests.py
+++ ugc/tests.py
@@ -1,14 +1,32 @@
 from django.test import TestCase
 from ugc.models import Post, Comment
 from core.models import User
+from like.models import Like, Likeable
+from django.contrib.contenttypes.models import ContentType
 
 class TestUGC(TestCase):
 
     def setUp(self):
         self.testuser = User.objects.create()
-        self.testpost = Post.objects.create(author=testuser)
+        self.testpost = Post.objects.create(author=self.testuser)
+        self.comment = Comment(author=self.testuser, post=self.testpost, text="asdf")
+        self.comment.save()
 
+    def test_comment_count(self):
+        assert self.testpost.comment_count == 1
+
+    def test_like_count(self):
+        self.testpost1 = Post(author=self.testuser)
+        self.testpost1.save()
+        self.testlike = Like(author=self.testuser,
+            target=self.testpost1,
+            target_id=self.testpost1.pk,
+            power=100,
+            target_content_type=ContentType.objects.get_for_model(Like))
+        self.testlike.save()
+        assert self.testpost1.like_count == 1
 
     def tearDown(self):
+        self.comment.delete()
         self.testuser.delete()
         self.testpost.delete()
